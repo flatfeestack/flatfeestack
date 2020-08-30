9d8a822efb29d46708caccea769cdd519439bf6f
calvin-f <34093317+calvin-f@users.noreply.github.com>
2020-08-30 17:22:38 +0200

    make go routines as well for weight analysis

--- analyzer/api.go
+++ analyzer/api.go
@@ -21,7 +21,6 @@ type PlatformInformationChannel struct {
 }
 
 func getAllContributions(w http.ResponseWriter, r *http.Request) {
-
 	var err error
 
 	// get the repository url from the request
@@ -149,73 +148,109 @@ func getAllContributions(w http.ResponseWriter, r *http.Request) {
 }
 
 func getContributionWeights(w http.ResponseWriter, r *http.Request) {
-	repositoryUrl := r.URL.Query()["repositoryUrl"]
-	if len(repositoryUrl) < 1 {
-		w.WriteHeader(http.StatusNotFound)
-		_, fmtErr := fmt.Fprintf(w, "Repository not found")
-		if fmtErr != nil {
-			fmt.Println("Could not format", fmtErr)
-		}
-		return
-	}
-
 	var err error
 
-	commitsSinceString := r.URL.Query()["since"]
-	var commitsSince time.Time
-	if len(commitsSinceString) > 0 {
-		commitsSince, err = convertTimestampStringToTime(commitsSinceString[0])
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			_, fmtErr := fmt.Fprintf(w, err.Error())
-			if fmtErr != nil {
-				fmt.Println("Could not format", fmtErr)
-			}
-			return
-		}
-
-	}
-
-	commitsUntilString := r.URL.Query()["until"]
-	var commitsUntil time.Time
-	if len(commitsUntilString) > 0 {
-		commitsUntil, err = convertTimestampStringToTime(commitsUntilString[0])
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			_, fmtErr := fmt.Fprintf(w, err.Error())
-			if fmtErr != nil {
-				fmt.Println("Could not format", fmtErr)
-			}
-			return
-		}
-	}
-
-	contributionMap, err := analyzeRepository(repositoryUrl[0], commitsSince, commitsUntil)
+	// get the repository url from the request
+	repositoryUrl, err := getRepositoryFromRequest(r)
 	if err != nil {
-		if strings.Contains(err.Error(), "authentication") {
-			w.WriteHeader(http.StatusUnauthorized)
-			_, fmtErr := fmt.Fprintf(w, err.Error())
-			if fmtErr != nil {
-				fmt.Println("Could not format", fmtErr)
-			}
-		} else {
-			w.WriteHeader(http.StatusInternalServerError)
-			_, fmtErr := fmt.Fprintf(w, err.Error())
-			if fmtErr != nil {
-				fmt.Println("Could not format", fmtErr)
-			}
-		}
+		makeHttpStatusErr(w, err.Error(), http.StatusNotFound)
 		return
 	}
+
+	// detect whether the platformInformation flag in the request was set
+	analyzePlatformInformation := getShouldAnalyzePlatformInformation(r)
+
+	commitsSince, commitsUntil, err := getTimeRange(r)
+	if err != nil {
+		makeHttpStatusErr(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// make the channels for both go routines (analyze repo / platform information)
+	gitAnalyzationChannel := make(chan GitAnalyzationChannel)
+	platformInformationChannel := make(chan PlatformInformationChannel)
+
+	// if we don't have to analyze the platform, close the channel again since we don't need it
+	if !analyzePlatformInformation {
+		close(platformInformationChannel)
+	}
+
+	// go routine to analyze the repository using git independently from main thread
+	go func() {
+		routineContributionMap, routineErr := analyzeRepository(repositoryUrl, commitsSince, commitsUntil)
+		gitAnalyzationChannel <- GitAnalyzationChannel{
+			Result: routineContributionMap,
+			Reason: routineErr,
+		}
+		close(gitAnalyzationChannel)
+	}()
+
+	// execute go routine to fetch the platform information only when the platformInformation flag is set
+	if analyzePlatformInformation {
+		go func() {
+			routineIssues, routinePullRequests, routineErr := getPlatformInformation(repositoryUrl, commitsSince, commitsUntil)
+			platformInformationChannel <- PlatformInformationChannel{
+				ResultIssues: routineIssues,
+				ResultPullRequests: routinePullRequests,
+				Reason: routineErr,
+			}
+			close(platformInformationChannel)
+		}()
+	}
+
+	// set the openness of the to the default value
+	chanel1Open := true
+	chanel2Open := analyzePlatformInformation
+
+	// initialize the return variables for the go routines
+	var contributionMap map[Contributor]Contribution
+	var issues []GQLIssue
+	var pullRequests []GQLPullRequest
+
+	// wait for the results of both go routines
+	for chanel1Open || chanel2Open {
+		select {
+		case msg1, ok1 := <-gitAnalyzationChannel:
+			if !ok1 {
+				// if the channel is closed set the flag to false
+				chanel1Open = false
+			} else if msg1.Reason != nil {
+				// error handling
+				if strings.Contains(msg1.Reason.Error(), "authentication") {
+					makeHttpStatusErr(w, msg1.Reason.Error(), http.StatusUnauthorized)
+				} else {
+					makeHttpStatusErr(w, msg1.Reason.Error(), http.StatusInternalServerError)
+				}
+				return
+			} else {
+				// save the return value to the initialized variable
+				contributionMap = msg1.Result
+			}
+		case msg2, ok2 := <-platformInformationChannel:
+			if !ok2 {
+				// if the channel is closed set the flag to false
+				chanel2Open = false
+			} else if msg2.Reason != nil {
+				// error handling
+				makeHttpStatusErr(w, msg2.Reason.Error(), http.StatusInternalServerError)
+				return
+			} else {
+				// save the return value to the initialized variable
+				issues = msg2.ResultIssues
+				pullRequests = msg2.ResultPullRequests
+			}
+		}
+	}
+
+	// only temporary since we don't include platform information in the weights yet
+	fmt.Println(issues)
+	fmt.Println(pullRequests)
 
 	weightsMap, err := weightContributions(contributionMap)
 
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		_, fmtErr := fmt.Fprintf(w, err.Error())
-		if fmtErr != nil {
-			fmt.Println("Could not format", fmtErr)
-		}
+		makeHttpStatusErr(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	var contributionWeights []FlatFeeWeight
