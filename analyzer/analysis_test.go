1d4623bb2ac3ec89ad24724790fb89af50f5c70f
Calvin Falter <calvin.falter@gmail.com>
2020-09-29 16:46:04 +0200

    add test for platforminformation weight

--- analyzer/analysis_test.go
+++ analyzer/analysis_test.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"archive/zip"
 	"fmt"
 	"github.com/go-git/go-git/v5"
 	"github.com/stretchr/testify/assert"
 	"io"
 	"os"
-	"archive/zip"
 	"path/filepath"
 	"strings"
 	"testing"
@@ -88,12 +88,12 @@ func TestAnalyzeRepositoryFromRepository(t *testing.T) {
 			Name:  "Claude Muller",
 			Email: "claude@axlabs.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 20297,
 			Deletion: 12134,
 		},
-		Merges:      11,
-		Commits:     143,
+		Merges:  11,
+		Commits: 143,
 	}
 	expectedContributions[Contributor{
 		Name:  "Claude Muller",
@@ -103,12 +103,12 @@ func TestAnalyzeRepositoryFromRepository(t *testing.T) {
 			Name:  "Claude Muller",
 			Email: "37138571+claudemiller@users.noreply.github.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 839,
 			Deletion: 652,
 		},
-		Merges:      5,
-		Commits:     4,
+		Merges:  5,
+		Commits: 4,
 	}
 	expectedContributions[Contributor{
 		Name:  "Guil. Sperb Machado",
@@ -118,12 +118,12 @@ func TestAnalyzeRepositoryFromRepository(t *testing.T) {
 			Name:  "Guil. Sperb Machado",
 			Email: "guil@axlabs.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 948,
 			Deletion: 398,
 		},
-		Merges:      18,
-		Commits:     0,
+		Merges:  18,
+		Commits: 0,
 	}
 	expectedContributions[Contributor{
 		Name:  "Guil. Sperb Machado",
@@ -133,12 +133,12 @@ func TestAnalyzeRepositoryFromRepository(t *testing.T) {
 			Name:  "Guil. Sperb Machado",
 			Email: "gsm@machados.org",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 0,
 			Deletion: 0,
 		},
-		Merges:      1,
-		Commits:     0,
+		Merges:  1,
+		Commits: 0,
 	}
 	expectedContributions[Contributor{
 		Name:  "Sebastian Stephan",
@@ -148,12 +148,12 @@ func TestAnalyzeRepositoryFromRepository(t *testing.T) {
 			Name:  "Sebastian Stephan",
 			Email: "sebastian-stephan@users.noreply.github.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 1,
 			Deletion: 1,
 		},
-		Merges:      0,
-		Commits:     1,
+		Merges:  0,
+		Commits: 1,
 	}
 	expectedContributions[Contributor{
 		Name:  "Nikita Andrejevs",
@@ -163,12 +163,12 @@ func TestAnalyzeRepositoryFromRepository(t *testing.T) {
 			Name:  "Nikita Andrejevs",
 			Email: "nimmortalz@gmail.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 3720,
 			Deletion: 1303,
 		},
-		Merges:      4,
-		Commits:     7,
+		Merges:  4,
+		Commits: 7,
 	}
 	expectedContributions[Contributor{
 		Name:  "Freddy Tuxworth",
@@ -178,12 +178,12 @@ func TestAnalyzeRepositoryFromRepository(t *testing.T) {
 			Name:  "Freddy Tuxworth",
 			Email: "freddytuxworth@gmail.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 47,
 			Deletion: 0,
 		},
-		Merges:      0,
-		Commits:     1,
+		Merges:  0,
+		Commits: 1,
 	}
 	expectedContributions[Contributor{
 		Name:  "Guilherme Sperb Machado",
@@ -193,12 +193,12 @@ func TestAnalyzeRepositoryFromRepository(t *testing.T) {
 			Name:  "Guilherme Sperb Machado",
 			Email: "guil@axlabs.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 39585,
 			Deletion: 14353,
 		},
-		Merges:      18,
-		Commits:     188,
+		Merges:  18,
+		Commits: 188,
 	}
 	expectedContributions[Contributor{
 		Name:  "Krain Chen",
@@ -208,12 +208,12 @@ func TestAnalyzeRepositoryFromRepository(t *testing.T) {
 			Name:  "Krain Chen",
 			Email: "chenquanyu@ngd.neo.org",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 8,
 			Deletion: 5,
 		},
-		Merges:      0,
-		Commits:     1,
+		Merges:  0,
+		Commits: 1,
 	}
 	expectedContributions[Contributor{
 		Name:  "Nikita Andrejevs",
@@ -223,12 +223,12 @@ func TestAnalyzeRepositoryFromRepository(t *testing.T) {
 			Name:  "Nikita Andrejevs",
 			Email: "nikita.andrejevs@knowledgeprice.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 273,
 			Deletion: 55,
 		},
-		Merges:      2,
-		Commits:     0,
+		Merges:  2,
+		Commits: 0,
 	}
 	expectedContributions[Contributor{
 		Name:  "施鹏",
@@ -238,12 +238,12 @@ func TestAnalyzeRepositoryFromRepository(t *testing.T) {
 			Name:  "施鹏",
 			Email: "shipeng@aladingbank.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 4,
 			Deletion: 2,
 		},
-		Merges:      0,
-		Commits:     1,
+		Merges:  0,
+		Commits: 1,
 	}
 	expectedContributions[Contributor{
 		Name:  "claudemiller",
@@ -253,12 +253,12 @@ func TestAnalyzeRepositoryFromRepository(t *testing.T) {
 			Name:  "claudemiller",
 			Email: "claude@axlabs.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 314,
 			Deletion: 257,
 		},
-		Merges:      0,
-		Commits:     1,
+		Merges:  0,
+		Commits: 1,
 	}
 	expectedContributions[Contributor{
 		Name:  "Krain Chen",
@@ -268,12 +268,12 @@ func TestAnalyzeRepositoryFromRepository(t *testing.T) {
 			Name:  "Krain Chen",
 			Email: "ssssu8@qq.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 60,
 			Deletion: 3,
 		},
-		Merges:      1,
-		Commits:     0,
+		Merges:  1,
+		Commits: 0,
 	}
 
 	assert.Equal(t, expectedContributions, contributions)
@@ -302,12 +302,12 @@ func TestAnalyzeRepositoryFromRepository_DateRange(t *testing.T) {
 			Name:  "Nikita Andrejevs",
 			Email: "nimmortalz@gmail.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 474,
 			Deletion: 131,
 		},
-		Merges:      0,
-		Commits:     3,
+		Merges:  0,
+		Commits: 3,
 	}
 	expectedContributions[Contributor{
 		Name:  "Freddy Tuxworth",
@@ -317,12 +317,12 @@ func TestAnalyzeRepositoryFromRepository_DateRange(t *testing.T) {
 			Name:  "Freddy Tuxworth",
 			Email: "freddytuxworth@gmail.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 47,
 			Deletion: 0,
 		},
-		Merges:      0,
-		Commits:     1,
+		Merges:  0,
+		Commits: 1,
 	}
 	expectedContributions[Contributor{
 		Name:  "Guilherme Sperb Machado",
@@ -332,14 +332,13 @@ func TestAnalyzeRepositoryFromRepository_DateRange(t *testing.T) {
 			Name:  "Guilherme Sperb Machado",
 			Email: "guil@axlabs.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 3634,
 			Deletion: 457,
 		},
-		Merges:      0,
-		Commits:     34,
+		Merges:  0,
+		Commits: 34,
 	}
-
 
 	assert.Equal(t, expectedContributions, contributions)
 	assert.Equal(t, nil, err)
@@ -359,12 +358,12 @@ func TestWeightContributions(t *testing.T) {
 			Name:  "Claude Muller",
 			Email: "claude@axlabs.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 10245,
 			Deletion: 6405,
 		},
-		Merges:      6,
-		Commits:     64,
+		Merges:  6,
+		Commits: 64,
 	}
 	inputContributions[Contributor{
 		Name:  "Claude Muller",
@@ -374,12 +373,12 @@ func TestWeightContributions(t *testing.T) {
 			Name:  "Claude Muller",
 			Email: "37138571+claudemiller@users.noreply.github.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 668,
 			Deletion: 372,
 		},
-		Merges:      3,
-		Commits:     1,
+		Merges:  3,
+		Commits: 1,
 	}
 	inputContributions[Contributor{
 		Name:  "Guil. Sperb Machado",
@@ -389,12 +388,12 @@ func TestWeightContributions(t *testing.T) {
 			Name:  "Guil. Sperb Machado",
 			Email: "guil@axlabs.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 90,
 			Deletion: 25,
 		},
-		Merges:      3,
-		Commits:     0,
+		Merges:  3,
+		Commits: 0,
 	}
 	inputContributions[Contributor{
 		Name:  "Nikita Andrejevs",
@@ -404,12 +403,12 @@ func TestWeightContributions(t *testing.T) {
 			Name:  "Nikita Andrejevs",
 			Email: "nimmortalz@gmail.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 2116,
 			Deletion: 729,
 		},
-		Merges:      2,
-		Commits:     2,
+		Merges:  2,
+		Commits: 2,
 	}
 	inputContributions[Contributor{
 		Name:  "Guilherme Sperb Machado",
@@ -419,12 +418,12 @@ func TestWeightContributions(t *testing.T) {
 			Name:  "Guilherme Sperb Machado",
 			Email: "guil@axlabs.com",
 		},
-		Changes:     CommitChange{
+		Changes: CommitChange{
 			Addition: 2571,
 			Deletion: 1093,
 		},
-		Merges:      8,
-		Commits:     35,
+		Merges:  8,
+		Commits: 35,
 	}
 
 	outputScore, err := weightContributions(inputContributions)
@@ -483,6 +482,246 @@ func TestWeightContributions(t *testing.T) {
 
 	sumOfScores := 0.0
 
+	for k, v := range outputScore {
+		expected, found := expectedOutput[k]
+		sumOfScores += v.Weight
+		assert.Equal(t, true, found)
+		assert.Equal(t, fmt.Sprintf("%.12f", expected.Weight), fmt.Sprintf("%.12f", v.Weight))
+	}
+
+	for k, _ := range expectedOutput {
+		_, found := outputScore[k]
+		assert.Equal(t, true, found)
+	}
+	assert.Equal(t, 1.0, sumOfScores)
+	assert.Equal(t, nil, err)
+}
+
+func TestWeightContributionsWithPlatformInformation(t *testing.T) {
+
+	inputContributions := make(map[Contributor]ContributionWithPlatformInformation)
+
+	inputContributions[Contributor{
+		Name:  "Claude Muller",
+		Email: "claude@axlabs.com",
+	}] = ContributionWithPlatformInformation{
+		GitInformation: Contribution{
+			Contributor: Contributor{
+				Name:  "Claude Muller",
+				Email: "claude@axlabs.com",
+			},
+			Changes: CommitChange{
+				Addition: 10245,
+				Deletion: 6405,
+			},
+			Merges:  6,
+			Commits: 64},
+		PlatformInformation: PlatformUserInformation{
+			UserName:               "csmuller",
+			IssueInformation:       IssueUserInformation{
+				Author:    nil,
+				Commenter: 6,
+			},
+			PullRequestInformation: PullRequestUserInformation{
+				Author:   []PullRequestInformation{{
+					State:   "MERGED",
+					Reviews: nil,
+				}, {
+					State:   "MERGED",
+					Reviews: nil,
+				}, {
+					State:   "MERGED",
+					Reviews: []string{
+						"CHANGES_REQUESTED",
+						"COMMENTED",
+						"APPROVED",
+					},
+				}, {
+					State:   "CLOSED",
+					Reviews: nil,
+				}, {
+					State:   "MERGED",
+					Reviews: nil,
+				}, {
+					State:   "MERGED",
+					Reviews: []string{
+						"COMMENTED",
+					},
+				}},
+				Reviewer: 1,
+			},
+		},
+	}
+	inputContributions[Contributor{
+		Name:  "Claude Muller",
+		Email: "37138571+claudemiller@users.noreply.github.com",
+	}] = ContributionWithPlatformInformation{
+		GitInformation: Contribution{
+			Contributor: Contributor{
+				Name:  "Claude Muller",
+				Email: "37138571+claudemiller@users.noreply.github.com",
+			},
+			Changes: CommitChange{
+				Addition: 668,
+				Deletion: 372,
+			},
+			Merges:  3,
+			Commits: 1},
+		PlatformInformation: PlatformUserInformation{
+			UserName:               "",
+			IssueInformation:       IssueUserInformation{
+				Author:    nil,
+				Commenter: 0,
+			},
+			PullRequestInformation: PullRequestUserInformation{
+				Author:   nil,
+				Reviewer: 0,
+			},
+		},
+	}
+	inputContributions[Contributor{
+		Name:  "Guil. Sperb Machado",
+		Email: "guil@axlabs.com",
+	}] = ContributionWithPlatformInformation{
+		GitInformation: Contribution{
+			Contributor: Contributor{
+				Name:  "Guil. Sperb Machado",
+				Email: "guil@axlabs.com",
+			},
+			Changes: CommitChange{
+				Addition: 90,
+				Deletion: 25,
+			},
+			Merges:  3,
+			Commits: 0},
+		PlatformInformation: PlatformUserInformation{
+			UserName:               "",
+			IssueInformation:       IssueUserInformation{
+				Author:    nil,
+				Commenter: 0,
+			},
+			PullRequestInformation: PullRequestUserInformation{
+				Author:   nil,
+				Reviewer: 0,
+			},
+		},
+	}
+	inputContributions[Contributor{
+		Name:  "Nikita Andrejevs",
+		Email: "nimmortalz@gmail.com",
+	}] = ContributionWithPlatformInformation{
+		GitInformation: Contribution{
+			Contributor: Contributor{
+				Name:  "Nikita Andrejevs",
+				Email: "nimmortalz@gmail.com",
+			},
+			Changes: CommitChange{
+				Addition: 2116,
+				Deletion: 729,
+			},
+			Merges:  2,
+			Commits: 2},
+		PlatformInformation: PlatformUserInformation{
+			UserName:               "nimmortal",
+			IssueInformation:       IssueUserInformation{
+				Author:    []int{1},
+				Commenter: 0,
+			},
+			PullRequestInformation: PullRequestUserInformation{
+				Author:   []PullRequestInformation{{
+					State:   "MERGED",
+					Reviews: nil,
+				}, {
+					State:   "CLOSED",
+					Reviews: nil,
+				}},
+				Reviewer: 0,
+			},
+		},
+	}
+	inputContributions[Contributor{
+		Name:  "Guilherme Sperb Machado",
+		Email: "guil@axlabs.com",
+	}] = ContributionWithPlatformInformation{
+		GitInformation: Contribution{
+			Contributor: Contributor{
+				Name:  "Guilherme Sperb Machado",
+				Email: "guil@axlabs.com",
+			},
+			Changes: CommitChange{
+				Addition: 2571,
+				Deletion: 1093,
+			},
+			Merges:  8,
+			Commits: 35},
+		PlatformInformation: PlatformUserInformation{
+			UserName:               "gsmachado",
+			IssueInformation:       IssueUserInformation{
+				Author:    []int{1,1,1,1,1,0},
+				Commenter: 5,
+			},
+			PullRequestInformation: PullRequestUserInformation{
+				Author:   nil,
+				Reviewer: 3,
+			},
+		},
+	}
+
+	outputScore, err := weightContributionsWithPlatformInformation(inputContributions)
+
+	expectedOutput := make(map[Contributor]FlatFeeWeight)
+	expectedOutput[Contributor{
+		Name:  "Claude Muller",
+		Email: "claude@axlabs.com",
+	}] = FlatFeeWeight{
+		Contributor: Contributor{
+			Name:  "Claude Muller",
+			Email: "claude@axlabs.com",
+		},
+		Weight: 0.6234323728996527,
+	}
+	expectedOutput[Contributor{
+		Name:  "Claude Muller",
+		Email: "37138571+claudemiller@users.noreply.github.com",
+	}] = FlatFeeWeight{
+		Contributor: Contributor{
+			Name:  "Claude Muller",
+			Email: "37138571+claudemiller@users.noreply.github.com",
+		},
+		Weight: 0.028603172335762865,
+	}
+	expectedOutput[Contributor{
+		Name:  "Guil. Sperb Machado",
+		Email: "guil@axlabs.com",
+	}] = FlatFeeWeight{
+		Contributor: Contributor{
+			Name:  "Guil. Sperb Machado",
+			Email: "guil@axlabs.com",
+		},
+		Weight: 0.0057419793144027126,
+	}
+	expectedOutput[Contributor{
+		Name:  "Nikita Andrejevs",
+		Email: "nimmortalz@gmail.com",
+	}] = FlatFeeWeight{
+		Contributor: Contributor{
+			Name:  "Nikita Andrejevs",
+			Email: "nimmortalz@gmail.com",
+		},
+		Weight: 0.10244416390659761,
+	}
+	expectedOutput[Contributor{
+		Name:  "Guilherme Sperb Machado",
+		Email: "guil@axlabs.com",
+	}] = FlatFeeWeight{
+		Contributor: Contributor{
+			Name:  "Guilherme Sperb Machado",
+			Email: "guil@axlabs.com",
+		},
+		Weight: 0.2397783115435842,
+	}
+
+	sumOfScores := 0.0
 	for k, v := range outputScore {
 		expected, found := expectedOutput[k]
 		sumOfScores += v.Weight
