dcc892b712381effe0850c31b2a9bed1ea93388c
Jonas Brunner <jonas.brunner@outlook.com>
2020-12-11 18:00:39 +0100

    update init sql

--- backend/init.sql
+++ backend/init.sql
@@ -10,6 +10,12 @@ EXCEPTION
     WHEN duplicate_object THEN null;
 END $$;
 
+DO $$ BEGIN
+    create type repobalanceevent as enum ('DISTRIBUTED');
+EXCEPTION
+    WHEN duplicate_object THEN null;
+END $$;
+
 create table if not exists "user"
 (
     id                 uuid    not null
@@ -63,33 +69,33 @@ create unique index if not exists repo_url_uindex
 
 create table if not exists git_user_balance
 (
-    id         serial    not null
+    id         serial                  not null
         constraint gituserbalance_pk
             primary key,
-    repo_id    integer   not null
+    repo_id    integer                 not null
         constraint repo_id
             references repo,
     uid        uuid
         constraint uid
             references "user",
-    balance    integer   not null,
-    created_at timestamp not null,
-    git_email  varchar,
-    score      integer
+    balance    numeric                 not null,
+    created_at timestamp default now() not null,
+    git_email  varchar                 not null,
+    score      numeric                 not null
 );
 
 comment on table git_user_balance is 'if the the uid is null, there is no registered user which owns the git_email ';
 
 create table if not exists git_userbalanceevent
 (
-    id                  serial           not null
+    id                  serial                  not null
         constraint gituserbalanceevent_pk
             primary key,
-    git_user_balance_id integer          not null
+    git_user_balance_id integer                 not null
         constraint "gitUserBalanceId"
             references git_user_balance,
-    timestamp           timestamp        not null,
-    type                userbalanceevent not null
+    timestamp           timestamp default now() not null,
+    type                userbalanceevent        not null
 );
 
 create unique index if not exists gituserbalanceevent_id_uindex
@@ -115,10 +121,10 @@ create table if not exists repo_balance
     id        serial  not null
         constraint repobalance_pk
             primary key,
-    repo_id   integer
+    repo_id   integer not null
         constraint "repoId"
             references repo,
-    balance   integer not null,
+    balance   numeric not null,
     timestamp timestamp
 );
 
@@ -131,23 +137,7 @@ create table if not exists daily_repo_balance
         constraint "repoId"
             references repo,
     timestamp date    not null,
-    balance   integer not null
-);
-
-create table if not exists contribution
-(
-    id             serial    not null
-        constraint contribution_pk
-            primary key,
-    git_email      varchar   not null,
-    git_name       varchar   not null,
-    computed_at    timestamp not null,
-    from_timestamp timestamp not null,
-    to_timestamp   timestamp not null,
-    repo_id        integer   not null
-        constraint repo
-            references repo,
-    branch         varchar
+    balance   numeric not null
 );
 
 create table if not exists payments
@@ -175,6 +165,46 @@ create table if not exists analysis_request
         constraint analysis_request_repo_id_fk
             references repo,
     branch  text
+);
+
+create table if not exists contribution
+(
+    id               serial    not null
+        constraint contribution_pk
+            primary key,
+    git_email        varchar   not null,
+    computed_at      timestamp not null,
+    weight           real      not null,
+    analysis_request uuid      not null
+        constraint contribution_analysis_request_fkey
+            references analysis_request
+            on delete cascade
+);
+
+create table if not exists payouts
+(
+    id        serial not null
+        constraint payouts_pkey
+            primary key,
+    uid       uuid   not null
+        constraint payouts_uid_fkey
+            references "user"
+            on delete cascade,
+    amount    numeric,
+    fulfilled boolean default false
+);
+
+create table if not exists repo_balance_event
+(
+    id           serial not null
+        constraint repo_balance_event_pkey
+            primary key,
+    repo_balance integer
+        constraint repo_balance_event_repo_balance_fkey
+            references repo_balance
+            on delete cascade,
+    type         repobalanceevent,
+    timestamp    timestamp default now()
 );
 
 create or replace view sponsored_repos(repo_id, user_id) as
@@ -205,21 +235,6 @@ create or replace function days_in_month(date) returns integer
 as
 $$
 select CAST(to_char(date_trunc('month', $1) + interval '1 month' - date_trunc('month', $1), 'dd') as integer)
-$$;
-
-create or replace function get_daily_repo_balance_at(date)
-    returns TABLE
-            (
-                repo_id         integer,
-                sponsor_amounts double precision
-            )
-    language sql
-as
-$$
-SELECT repo_id,
-       (get_monthly_sponsor_amount(user_id) / days_in_month($1)) * 1000 /
-       (countsponsoredrepos_at(user_id, $1))::FLOAT as sponsor_amounts
-FROM get_sponsored_repos_at($1)
 $$;
 
 create or replace function get_sponsored_repos_at(date)
@@ -343,8 +358,96 @@ create or replace function get_daily_repo_balance()
 as
 $$
 SELECT repo_id,
-       (get_monthly_sponsor_amount(user_id) / days_in_month(now()::date)) * 1000 /
-       (countsponsoredrepos(user_id))::FLOAT as sponsor_amounts
+       ROUND(((get_monthly_sponsor_amount(user_id)::FLOAT / days_in_month(now()::date)) /
+              (countsponsoredrepos(user_id))::FLOAT)::NUMERIC, 3) as sponsor_amounts
 FROM sponsored_repos
 $$;
+
+create or replace function get_unclaimed_balances()
+    returns TABLE
+            (
+                id        integer,
+                uid       uuid,
+                git_email character varying,
+                balance   numeric
+            )
+    language sql
+as
+$$
+(SELECT b.id, b.uid, b.git_email, b.balance
+ FROM git_user_balance b
+ WHERE NOT EXISTS
+     (SELECT * FROM git_userbalanceevent e WHERE b.id = e.git_user_balance_id)
+   AND uid IS NOT NULL)
+$$;
+
+create or replace function get_expired_balances()
+    returns TABLE
+            (
+                id      integer,
+                uid     uuid,
+                repo_id integer,
+                balance numeric
+            )
+    language sql
+as
+$$
+(SELECT b.id, b.uid, b.repo_id, b.balance
+ FROM git_user_balance b
+ WHERE NOT EXISTS
+     (SELECT * FROM git_userbalanceevent e WHERE b.id = e.git_user_balance_id)
+   AND uid IS NULL
+   AND created_at < now() - '6 MONTHS'::interval)
+$$;
+
+create or replace function get_daily_repo_balance_at(date)
+    returns TABLE
+            (
+                repo_id integer,
+                balance double precision
+            )
+    language sql
+as
+$$
+SELECT repo_id,
+       ROUND(((get_monthly_sponsor_amount_at(user_id, $1)::FLOAT / days_in_month($1)) /
+              (countsponsoredrepos_at(user_id, $1))::FLOAT)::NUMERIC, 3) as sponsor_amounts
+FROM get_sponsored_repos_at($1)
+WHERE get_monthly_sponsor_amount_at(user_id, $1) is not NULL
+$$;
+
+create or replace function latest_repo_balance_at(date)
+    returns TABLE
+            (
+                id          integer,
+                repo_id     integer,
+                "timestamp" date,
+                balance     numeric
+            )
+    language sql
+as
+$$
+SELECT drb.*
+FROM (SELECT MAX(id) as id FROM daily_repo_balance WHERE timestamp = now()::date GROUP BY repo_id) as latest
+         JOIN daily_repo_balance drb ON latest.id = drb.id
+
+$$;
+
+create or replace function check_daily_repo_balance_insert() returns trigger
+    language plpgsql
+as
+$$
+BEGIN
+    IF (Select count(id) FROM daily_repo_balance WHERE repo_id = NEW.repo_id and "timestamp" = NEW."timestamp") > 0 THEN
+        RAISE EXCEPTION 'Already inserted daily repo balance for this day';
+    END IF;
+    RETURN NEW;
+END;
+$$;
+
+create trigger check_daily_repo_balance_insert
+    before insert or update
+    on daily_repo_balance
+    for each row
+execute procedure check_daily_repo_balance_insert();
 
