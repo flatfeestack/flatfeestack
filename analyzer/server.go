b9fde18a7978824dd7cce9c45ccf2aeadbfcf031
Thomas Bocek <tom@tomp2p.net>
2021-01-01 20:33:37 +0100

    Devops stuff

--- analyzer/server.go
+++ analyzer/server.go
@@ -1,13 +1,59 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
+var (
+	opts *Opts
+)
+
+type Opts struct {
+	Port int
+}
+func NewOpts() *Opts {
+	o := &Opts{}
+	flag.IntVar(&o.Port, "port", LookupEnvInt("PORT"), "listening HTTP port")
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+	return o
+}
+
+func setDefaultInt(actualValue int, defaultValue int) int {
+	if actualValue == 0 {
+		return defaultValue
+	}
+	return actualValue
+}
+
+func LookupEnvInt(key string) int {
+	if val, ok := os.LookupEnv(key); ok {
+		v, err := strconv.Atoi(val)
+		if err != nil {
+			log.Printf("LookupEnvOrInt[%s]: %v", key, err)
+			return 0
+		}
+		return v
+	}
+	return 0
+}
+
+func defaultOpts(opts *Opts) {
+	opts.Port = setDefaultInt(opts.Port, 8080)
+}
+
 func main() {
+	opts = NewOpts()
+	defaultOpts(opts)
 	err := setEnvs()
 	if err != nil {
 		fmt.Println(err)
@@ -19,5 +65,5 @@ func main() {
 	router.HandleFunc("/contributions", getAllContributions).Methods("GET")
 	router.HandleFunc("/weights", getContributionWeights).Methods("GET")
 	router.HandleFunc("/webhook", analyzeRepository).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(opts.Port), router))
 }
