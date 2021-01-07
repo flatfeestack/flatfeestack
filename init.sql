9ab00fcad61cf344062ad7742f1e832205a6a95b
Thomas Bocek <tom@tomp2p.net>
2021-01-07 19:31:20 +0100

    adapted SQL script

--- init.sql
+++ init.sql
@@ -4,22 +4,24 @@ CREATE TABLE users (
     email              VARCHAR(255) UNIQUE NOT NULL,
     subscription       VARCHAR(255),
     subscription_state VARCHAR(255),
-    payout_eth         VARCHAR(255)
+    payout_eth         VARCHAR(255),
+    created_at         TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE repo (
     id          UUID PRIMARY KEY,
-    orig_id     INTEGER,
-    orig_from   VARCHAR(255),
+    orig_id     NUMERIC,
     url         VARCHAR(255) UNIQUE NOT NULL,
     name        VARCHAR(255)        NOT NULL,
-    description TEXT
+    description TEXT,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE git_email (
-    id      UUID PRIMARY KEY,
-    user_id UUID CONSTRAINT fk_user_id_gub REFERENCES users (id),
-    email   VARCHAR(255) UNIQUE NOT NULL
+    id         UUID PRIMARY KEY,
+    user_id    UUID CONSTRAINT fk_user_id_gub REFERENCES users (id),
+    email      VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 -- if the the uid is null, there is no registered user which owns the git_email
@@ -63,20 +65,22 @@ CREATE TABLE daily_repo_balance (
 );
 
 CREATE TABLE payments (
-    id        UUID PRIMARY KEY,
-    user_id   UUID CONSTRAINT fk_user_id_p REFERENCES users (id),
-    date_from TIMESTAMP NOT NULL,
-    date_to   TIMESTAMP,
-    sub       TEXT,
-    amount    INTEGER
+    id         UUID PRIMARY KEY,
+    user_id    UUID CONSTRAINT fk_user_id_p REFERENCES users (id),
+    date_from  TIMESTAMP NOT NULL,
+    date_to    TIMESTAMP,
+    sub        TEXT,
+    amount     BIGINT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE analysis_request (
-    id        UUID PRIMARY KEY,
-    repo_id   UUID CONSTRAINT fk_repo_id_ar REFERENCES repo (id),
-    date_from TIMESTAMP NOT NULL,
-    date_to   TIMESTAMP NOT NULL,
-    branch    TEXT
+    id         UUID PRIMARY KEY,
+    repo_id    UUID CONSTRAINT fk_repo_id_ar REFERENCES repo (id),
+    date_from  TIMESTAMP NOT NULL,
+    date_to    TIMESTAMP NOT NULL,
+    branch     TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE contribution (
@@ -84,7 +88,8 @@ CREATE TABLE contribution (
     analysis_request_id UUID CONSTRAINT fk_analysis_request_id_c REFERENCES analysis_request (id) ON DELETE CASCADE,
     git_email           VARCHAR(255)     NOT NULL,
     computed_at         TIMESTAMP        NOT NULL,
-    weight              DOUBLE PRECISION NOT NULL
+    weight              DOUBLE PRECISION NOT NULL,
+    created_at          TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE repo_balance_event (
@@ -96,8 +101,7 @@ CREATE TABLE repo_balance_event (
 
 CREATE TABLE exchange (
     id         UUID PRIMARY KEY,
-    amount     NUMERIC,
-    chain_id   VARCHAR(255)                        NOT NULL,
+    chain      VARCHAR(255) NOT NULL,
     price      NUMERIC,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
