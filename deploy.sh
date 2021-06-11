3cb184b252666cff62ff98fb934245f97ff160af
Thomas Bocek <tom@tomp2p.net>
2021-06-11 19:52:09 +0200

    deploy to test

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,6 @@ git submodule foreach 'git pull'
 git add analysis-engine backend fastauth frontend payout search-proj
 git commit -m "update to latest"
 git push --recurse-submodules=on-demand
+CURRENT=`git tag --sort=taggerdate | tail -1`
+git tag "`semver $CURRENT -i patch`"
+git push origin `semver $CURRENT -i patch`
