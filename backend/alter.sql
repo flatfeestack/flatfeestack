b44be818232304c5155d218ced1bfe01189d00e3
Thomas Bocek <tom@tomp2p.net>
2021-06-25 19:25:23 +0200

    added alter table scripts

--- backend/alter.sql
+++ backend/alter.sql
@@ -1,4 +1,4 @@
---22.5.2021
+--22.05.2021
 
 CREATE table daily_user_contribution(
                                         id                  UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
@@ -15,3 +15,7 @@ CREATE table daily_user_contribution(
 CREATE UNIQUE INDEX daily_user_contribution_index ON daily_user_contribution(user_id, repo_id, contributor_email, day);
 
 CREATE UNIQUE INDEX analysis_response_index ON analysis_response(analysis_request_id, git_email);
+
+--25.06.2021
+alter table analysis_response add git_name VARCHAR(255);
+alter table users add invited_email VARCHAR(64);
