e0323076c7e5ede7815452d42ed9b875bdab1cfa
Thomas Bocek <tom@tomp2p.net>
2021-06-25 17:36:43 +0200

    add proper sorting

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,11 @@
 ##pulls to the latest version
 #git submodule update --recursive --remote
 ##or
+git fetch
 git submodule foreach 'git pull'
 git add analysis-engine backend fastauth frontend payout search-proj
 git commit -m "update to latest"
 git push --recurse-submodules=on-demand
-CURRENT=`git tag --sort=taggerdate | tail -1`
+CURRENT=`git tag --sort=creatordate | tail -1`
 git tag "`semver $CURRENT -i patch`"
 git push origin `semver $CURRENT -i patch`
