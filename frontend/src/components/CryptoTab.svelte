240ae8fd5327da4f3cd8c47a89bb9e1aa240101c
Marco Endres <marco.endres@ost.ch>
2021-10-21 15:00:07 +0200

    Implement PoC for different selection methods

---
+++ frontend/src/components/CryptoTab.svelte
@@ -0,0 +1,40 @@
+<script>
+    let questions = [
+        { id: 1, name: `Ethereum` },
+        { id: 2, name: `NEO` },
+        { id: 3, name: `Tezos` }
+    ];
+
+    let selected;
+
+    async function handleSubmit() {
+        alert(selected.name);
+        let url = 'http://...'
+        const response = await fetch(url, {
+            method: 'POST'})
+    }
+</script>
+
+<h2>Select your cryptocurrency</h2>
+
+<form on:submit|preventDefault={handleSubmit}>
+    <select bind:value={selected}>
+        {#each questions as question}
+            <option value={question}>
+                {question.name}
+            </option>
+        {/each}
+    </select>
+
+    <button disabled={!selected} type=submit>
+        Submit
+    </button>
+</form>
+
+<style>
+    input {
+        display: block;
+        width: 500px;
+        max-width: 100%;
+    }
+</style>
---
+++ frontend/src/components/PaymentSelection.svelte
@@ -0,0 +1,19 @@
+<script>
+    import FiatTab from "./FiatTab.svelte";
+    import CryptoTab from "./CryptoTab.svelte";
+    import Tabs from "./Tabs.svelte";
+
+    // List of tab items with labels, values and assigned components
+    let items = [
+        { label: "Fiat",
+            value: 1,
+            component: FiatTab
+        },
+        { label: "Crypto",
+            value: 2,
+            component: CryptoTab
+        }
+    ];
+</script>
+
+<Tabs {items} />
