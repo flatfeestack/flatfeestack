3b58d193c32a914fddac75790466ec13594f5003
Michael Bucher <michael@axlabs.com>
2021-11-16 14:26:17 +0100

    Update documentation of Flatfeestack.sol contract

--- payout/Flatfeestack.sol
+++ payout/Flatfeestack.sol
@@ -2,8 +2,14 @@ pragma solidity ^0.8.4;
 
 contract PayoutEth {
 
-    // Maps the developer's addresses to their total earned amounts (teas).
+    /**
+    * @dev Maps each address to its current total earned amount (tea).
+    */
     mapping(address => uint256) public teaMap;
+
+    /**
+    * @dev The contract owner
+    */
     address public owner;
 
     constructor () {
@@ -13,31 +19,36 @@ contract PayoutEth {
     receive() external payable {
     }
 
-    // Changes the contract owner.
-    // Keep in mind, that changing the contract owner invalidates all previously generated signatures for the withdraw
-    // method.
+    /**
+    * @dev Changes the owner of this contract.
+    */
     function changeOwner(address newOwner) public onlyOwner() {
         owner = newOwner;
     }
 
-    // Returns the value in the teaMap for the provided address.
+    /**
+    * @dev Gets the tea for the provided address.
+    */
     function getTea(address _dev) public view returns (uint256) {
         return teaMap[_dev];
     }
 
-    // Sets the value for the provided address to newTea. Requires the oldTea to match and the newTea to be greater
-    // than the current value.
+    /**
+    * @dev Sets the tea for the provided address. The oldTea must match the currently stored value, in order to verify
+    * that no immediate withdrawal took place before this is executed.
+    */
     function setTea(address _dev, uint256 oldTea, uint256 newTea) public onlyOwner() {
         require(oldTea == teaMap[_dev], "Stored tea is not equal to the provided oldTea.");
         require(newTea > teaMap[_dev], "Cannot set a lower value due to security reasons.");
         teaMap[_dev] = newTea;
     }
 
-    // Sets the values for the provided addresses to the corresponding newTeas.
-    // If the corresponding oldTea does not match the current value, no change is applied for this address.
-    function setTeas(address[] calldata _devs, uint256[] calldata oldTeas, uint256[] calldata newTeas)
-        public onlyOwner()
-    {
+    /**
+    * @dev Sets the teas for the provided addresses. The oldTeas must match the currently stored value, in order to
+    * verify that no immediate withdrawal took place before this is executed. In case one value does not match, no
+    * update is executed for that address.
+    */
+    function setTeas(address[] calldata _devs, uint256[] calldata oldTeas, uint256[] calldata newTeas) public onlyOwner() {
         require(_devs.length == newTeas.length, "Parameters must have same length.");
         for (uint256 i = 0; i < _devs.length; i++) {
             address dev = _devs[i];
@@ -49,8 +60,16 @@ contract PayoutEth {
         }
     }
 
-    // Transfers the amount _tea to address _dev, if the provided signature values match the created message and the
-    // contract owner.
+    /**
+    * @dev Withdraws the earned amount. The signature has to be created by the contract owner and the signed message
+    * is the hash of the concatenation of the account and tea.
+    *
+    * @param _dev The address to withdraw to.
+    * @param _tea The amount to withdraw.
+    * @param _v The recovery byte of the signature.
+    * @param _r The r value of the signature.
+    * @param _s The s value of the signature.
+    */
     function withdraw(address payable _dev, uint256 _tea, uint8 _v, bytes32 _r, bytes32 _s) public {
         require(_tea > teaMap[_dev], "These funds have already been withdrawn.");
         require(ecrecover(keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n66",
@@ -62,8 +81,14 @@ contract PayoutEth {
         _dev.transfer(_tea - oldTea);
     }
 
-    // Updates the teaMap with the _teas for the corresponding addresses _devs and transfers the difference of _teas
-    // to the current value in the teaMap to the corresponding _devs.
+    /**
+    * @dev Pays out the earned amount for multiple addresses. The payment amount for each address is equal to the
+    * difference of the provided new tea in the parameter and the currently stored value in the teaMap. After
+    * calculating the payment amount, the value of the account in the teaMap is updated with the new tea.
+    *
+    * @param _devs The addresses to pay out to.
+    * @param _teas The teas for the corresponding addresses.
+    */
     function batchPayout(address payable[] memory _devs, uint256[] memory _teas) public onlyOwner() {
         require(_devs.length == _teas.length, "Arrays must have same length.");
         for (uint256 i = 0; i < _devs.length; i++) {
