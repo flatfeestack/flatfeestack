7235605ed0ba194815f9c9f3becf65db5e4f3033
tbocek <tom@tomp2p.net>
2022-03-27 18:56:33 +0200

    remove unused function

--- update.sh
+++ update.sh
@@ -21,13 +21,6 @@ msg() {
   echo >&2 -e "${1-}"
 }
 
-die() {
-  local msg=$1
-  local code=${2-1} # default exit status 1
-  msg "$msg"
-  exit "$code"
-}
-
 setup_colors
 
 projects='analysis-engine backend fastauth frontend payout'
