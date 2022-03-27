b17e97890787671c7b918b790b0c3127ef518f20
tbocek <tom@tomp2p.net>
2022-03-27 18:42:11 +0200

    manual update

--- update.sh
+++ update.sh
@@ -1,4 +1,40 @@
-#!/bin/sh -e
+#!/usr/bin/env bash
 
-git submodule foreach 'git pull origin main'
-git submodule foreach --recursive 'git checkout main'
+# based on https://betterdev.blog/minimal-safe-bash-script-template/
+set -Eeuo pipefail
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+cleanup() {
+  trap - SIGINT SIGTERM ERR EXIT
+  # script cleanup here
+}
+
+setup_colors() {
+  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+  else
+    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+  fi
+}
+
+msg() {
+  echo >&2 -e "${1-}"
+}
+
+die() {
+  local msg=$1
+  local code=${2-1} # default exit status 1
+  msg "$msg"
+  exit "$code"
+}
+
+setup_colors
+
+projects='analysis-engine backend fastauth frontend payout'
+
+git pull
+for name in ${projects}; do
+  [ ! -d "$name" ] && git clone git@github.com:flatfeestack/"$name".git;git -C "$name" config pull.rebase false;
+  git -C "$name" pull
+  msg "${GREEN}[$(git -C "$name" symbolic-ref --short HEAD)]${NOFORMAT}-> $name"
+done
