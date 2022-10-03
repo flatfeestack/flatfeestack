007b7555da9bc2c899a9625bf8f0dcf81245636b
Andy Pfister <andy.pfister@hey.com>
2022-10-03 17:29:30 +0200

    Replace deprecated OpenZepplin dependency

--- smart-contracts-eth/contracts/Wallet.sol
+++ smart-contracts-eth/contracts/Wallet.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import "../node_modules/openzeppelin-solidity/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 contract Wallet is Ownable {
     uint256 public totalBalance;
