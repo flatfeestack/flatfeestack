6fe5c3c6074fde63878627b707fabb7cee3a6bb9
Pascal <pascal.knecht@hotmail.ch>
2022-10-18 13:47:14 +0200

    coverage

--- smart-contracts-eth/hardhat.config.ts
+++ smart-contracts-eth/hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import "solidity-coverage";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
