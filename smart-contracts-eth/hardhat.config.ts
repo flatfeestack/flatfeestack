7cf7d859770a16a96c3ffb27201b20dae9e716bb
Andy Pfister <andy.pfister@hey.com>
2022-10-31 13:14:29 +0100

    Add deploy and export scripts to local chain

--- smart-contracts-eth/hardhat.config.ts
+++ smart-contracts-eth/hardhat.config.ts
@@ -1,6 +1,8 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import "hardhat-deploy";
+import "hardhat-deploy-ethers";
+import { HardhatUserConfig } from "hardhat/config";
 import "solidity-coverage";
 
 const config: HardhatUserConfig = {
@@ -11,6 +13,27 @@ const config: HardhatUserConfig = {
         enabled: true,
         runs: 5,
       },
+    },
+  },
+  namedAccounts: {
+    representative: {
+      1337: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+    },
+    whitelisterOne: {
+      1337: "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+    },
+    whitelisterTwo: {
+      1337: "0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f",
+    },
+    member: {
+      1337: "0xFABB0ac9d68B0B445fB7357272Ff202C5651694a",
+    },
+  },
+  networks: {
+    localhost: {
+      live: false,
+      saveDeployments: true,
+      tags: ["local"],
     },
   },
 };
---
+++ smart-contracts-eth/scripts/exportInterfacesToFrontend.ts
@@ -0,0 +1,68 @@
+import {
+  readFileSync,
+  writeFileSync,
+  createWriteStream,
+  openSync,
+  closeSync,
+} from "fs";
+import { resolve } from "path";
+import { ethers } from "hardhat";
+
+async function main() {
+  const contractAddresses: String[] = [];
+  const pathToFrontend = process.env.FRONTEND_PATH ?? "../../frontend";
+
+  ["DAA", "Membership"].forEach(async (contractName: String) => {
+    const contractAbi = resolve(
+      __dirname,
+      `../artifacts/contracts/${contractName}.sol/${contractName}.json`
+    );
+
+    const file = readFileSync(contractAbi, "utf8");
+    const json = JSON.parse(file);
+    const abi = json.abi;
+
+    const iface = new ethers.utils.Interface(abi);
+    const resultFile = resolve(
+      __dirname,
+      `${pathToFrontend}/src/contracts/${contractName}.ts`
+    );
+
+    writeFileSync(
+      resultFile,
+      `export const ${contractName}ABI = ${JSON.stringify(
+        iface.format(ethers.utils.FormatTypes.full)
+      )}`
+    );
+
+    const contractProxyDeploymentFilePath = resolve(
+      __dirname,
+      `../deployments/localhost/${contractName}.json`
+    );
+    const contractProxyDeploymentFile = readFileSync(
+      contractProxyDeploymentFilePath,
+      "utf8"
+    );
+    const contractProxyDeployment = JSON.parse(contractProxyDeploymentFile);
+    contractAddresses.push(
+      `VITE_${contractName.toUpperCase()}_CONTRACT_ADDRESS=${
+        contractProxyDeployment.address
+      }`
+    );
+  });
+
+  const dotEnvFilePath = resolve(__dirname, `${pathToFrontend}/.env`);
+
+  // empty .env file
+  closeSync(openSync(dotEnvFilePath, "w"));
+  const dotEnvFile = createWriteStream(dotEnvFilePath);
+  contractAddresses.forEach(function (v) {
+    dotEnvFile.write(`${v}\n`);
+  });
+  dotEnvFile.end();
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
