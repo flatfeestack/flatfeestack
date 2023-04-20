2c6235fc70fff6349405714f62ac7fe371e634f2
Pascal <pascal.knecht@hotmail.ch>
2023-04-20 15:23:11 +0200

    database init

--- forum/init.sql
+++ forum/init.sql
@@ -1,1 +1,22 @@
--- TODO
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS post
+(
+    id         UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    author     UUID         NOT NULL,
+    content    TEXT         NOT NULL,
+    created_at TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
+    open       BOOLEAN      NOT NULL DEFAULT true,
+    title      VARCHAR(255) NOT NULL,
+    updated_at TIMESTAMPTZ
+);
+
+CREATE TABLE IF NOT EXISTS comment
+(
+    id         UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    author     UUID        NOT NULL,
+    content    TEXT        NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ,
+    post_id    UUID REFERENCES post (id) ON DELETE CASCADE
+);
