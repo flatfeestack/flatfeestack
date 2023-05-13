280dd9a65869b95d86711494488407a5dfb8fa4b
Pascal <pascal.knecht@hotmail.ch>
2023-05-13 16:53:48 +0200

    fix auth tests

--- fastauth/main_test.go
+++ fastauth/main_test.go
@@ -7,6 +7,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"flag"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/xlzd/gotp"
