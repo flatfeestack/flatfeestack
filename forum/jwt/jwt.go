69de2d8c841900dc07e046c5f0de39261a984ac5
Pascal <pascal.knecht@hotmail.ch>
2023-05-20 11:40:27 +0200

    refactor forum to retrieve user from backend

--- forum/jwt/jwt.go
+++ forum/jwt/jwt.go
@@ -2,14 +2,17 @@ package jwt
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"forum/api"
-	database "forum/db"
 	"forum/globals"
+	"forum/types"
 	"forum/utils"
 	"github.com/flatfeestack/go-lib/auth"
+	"github.com/go-jose/go-jose/v3/json"
 	log "github.com/sirupsen/logrus"
 	"net/http"
+	"time"
 )
 
 const (
@@ -57,28 +60,28 @@ func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
 			return
 		}
 
-		dbUser, err := database.FindUserByEmail(claims.Subject)
+		user, err := findUserByEmail(claims.Subject)
 		if err != nil {
 			utils.WriteErrorf(w, http.StatusBadRequest, "User find error: %v", err)
 			return
 		}
-		dbUser.Claims = *claims
+		user.Claims = *claims
 
 		log.Info(fmt.Sprintf("User [%s] request [%s]:%s\n", claims.Subject, r.URL, r.Method))
 
-		dbUser.Role = "User"
+		user.Role = "User"
 		for _, email := range globals.ADMINS {
 			if claims.Subject == email {
 				log.Info(fmt.Sprintf("Authenticated admin %s\n", email))
-				dbUser.Role = "Admin"
+				user.Role = "Admin"
 
 			}
 		}
-		ctx = context.WithValue(ctx, CurrentUser, dbUser)
+		ctx = context.WithValue(ctx, CurrentUser, user)
 
 		switch scopesSlice[0] {
 		case "Admin":
-			if dbUser.Role == "Admin" {
+			if user.Role == "Admin" {
 				next.ServeHTTP(w, r.WithContext(ctx))
 				return
 			}
@@ -91,4 +94,40 @@ func AuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
 			return
 		}
 	}
+}
+
+func findUserByEmail(email string) (*types.User, error) {
+	c := &http.Client{
+		Timeout: 15 * time.Second,
+	}
+
+	req, err := http.NewRequest(http.MethodGet, globals.OPTS.BackendUrl+"users/by/"+email, nil)
+	if err != nil {
+		log.Printf("Could not create a HTTP request to call the backend %v", err)
+		return nil, err
+	}
+
+	basicAuth := globals.OPTS.BackendUsername + ":" + globals.OPTS.BackendPassword
+	req.Header.Add("Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte(basicAuth)))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := c.Do(req)
+	if err != nil {
+		log.Printf("Error sending HTTP request: %v", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected response status: %d", resp.StatusCode)
+	}
+
+	var user types.User
+	err = json.NewDecoder(resp.Body).Decode(&user)
+	if err != nil {
+		log.Printf("Error decoding JSON response: %v", err)
+		return nil, err
+	}
+
+	return &user, nil
 }
--- forum/types/types.go
+++ forum/types/types.go
@@ -1,11 +1,27 @@
 package types
 
+import (
+	"github.com/go-jose/go-jose/v3/jwt"
+	"github.com/google/uuid"
+)
+
 type Opts struct {
-	Port      int
-	HS256     string
-	Env       string
-	DBPath    string
-	DBDriver  string
-	DBScripts string
-	Admins    string
+	Port            int
+	HS256           string
+	Env             string
+	DBPath          string
+	DBDriver        string
+	DBScripts       string
+	Admins          string
+	BackendUrl      string
+	BackendUsername string
+	BackendPassword string
+}
+
+type User struct {
+	Id     uuid.UUID `json:"id"`
+	Email  string    `json:"email"`
+	Name   string    `json:"name,omitempty"`
+	Claims jwt.Claims
+	Role   string `json:"role,omitempty"`
 }
