8253c1c5aa8e30b5127cbfed6e0361a4b6290d0a
Andy Pfister <andy.pfister@hey.com>
2023-05-26 13:48:41 +0200

    Add test for updating existing proposal

--- forum/dao/eventListener_test.go
+++ forum/dao/eventListener_test.go
@@ -85,4 +85,40 @@ Proposal description: %s`, proposer, "some text and "+description)
 			t.Errorf("there were unfulfilled expectations: %s", err)
 		}
 	})
+
+	t.Run("adds proposal id to existing discussion", func(t *testing.T) {
+		db, mock, err := sqlmock.New()
+		require.Nil(t, err)
+		globals.DB = db
+		defer db.Close()
+
+		require.Nil(t, err)
+		proposalId := big.NewInt(1234)
+		proposer := "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+		discussionId := uuid.New()
+		description := "Original discussion: http://localhost/dao/discussion/" + discussionId.String()
+
+		mock.ExpectQuery("SELECT EXISTS").WithArgs(discussionId).WillReturnRows(sqlmock.NewRows([]string{"exists"}).AddRow("true"))
+		mock.ExpectPrepare("SELECT id, author, content, created_at, open, title, updated_at, proposal_id")
+		mock.ExpectQuery("SELECT id, author, content, created_at, open, title, updated_at, proposal_id").WithArgs(discussionId).WillReturnRows(
+			sqlmock.NewRows([]string{"id", "author", "content", "created_at", "open", "title", "updated_at", "proposal_id"}).AddRow(
+				discussionId.String(), uuid.New(), "some content", time.Now(), true, "test title", nil, nil))
+		mock.ExpectPrepare("UPDATE post")
+		mock.ExpectQuery("UPDATE post").WithArgs(proposalId.String(), discussionId).WillReturnRows(
+			sqlmock.NewRows([]string{"author", "content", "created_at", "open", "title", "updated_at"}).
+				AddRow(uuid.New(), "some content", time.Now(), true, "test title", nil),
+		)
+
+		event := ContractDAOProposalCreated{
+			ProposalId:  proposalId,
+			Proposer:    common.HexToAddress(proposer),
+			Description: "some text and " + description,
+		}
+
+		LinkOrCreateDiscussion(event)
+
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("there were unfulfilled expectations: %s", err)
+		}
+	})
 }
