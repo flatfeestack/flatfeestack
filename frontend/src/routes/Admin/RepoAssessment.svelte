dbe96c84fa5ebd21b57a651d9d3bf5dbc194f5b6
Yannick Städeli <yannick.staedeli@ost.ch>
2024-11-27 08:06:36 +0100

    feature: create page for repo assessment, add it to navigation and index page

---
+++ frontend/src/routes/Admin/RepoAssessment.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+import Navigation from "../../components/Navigation.svelte";
+
+</script>
+
+<style>
+
+</style>
+
+<Navigation>
+  <h2 class="p-2 m-2">Repo Assessment</h2>
+</Navigation>
+
