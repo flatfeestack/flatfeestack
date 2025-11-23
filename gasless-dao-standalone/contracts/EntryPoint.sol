c98fe828cdeaa99f83872492f58058f03faf7f6e
nico.heiniger <nico.heiniger@gmx.ch>
2025-11-23 03:04:54 +0100

    remove failing hardhat gasless DAO tests and revert the DAO contract split

--- gasless-dao-standalone/contracts/EntryPoint.sol
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.23;
-
-import "@account-abstraction/contracts/core/EntryPointSimulations.sol";
-import "@account-abstraction/contracts/interfaces/PackedUserOperation.sol";
